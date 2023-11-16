c7ba43e5087f720dd977108dcac5ccf117f0ba4a
EDMerstratec <erme0001@edu.mercantec.dk>
2023-11-16 12:26:27 +0100

    0.3 formatting + submenu choices
    Prettier stopped working? Manually formatting right now
    Satisfied with mouseover submenu function
    Hash out vertical limit + scrollbar if needed

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,31 @@
 function displayAboutMe() {
+  document.getElementById("AboutMe").style.display = "";
+  document.getElementById("Cats").style.display = "none";
   document.getElementById("Projects").style.display = "none";
-  document.getElementById("AboutMe").style.display = "flex";
 }
 function displayProjects() {
   document.getElementById("AboutMe").style.display = "none";
-  document.getElementById("Projects").style.display = "flex";
+  document.getElementById("Projects").style.display = "";
+  document.getElementById("Cats").style.display = "none";
+}
+function displayCat() {
+  document.getElementById("AboutMe").style.display = "none";
+  document.getElementById("Projects").style.display = "none";
+  document.getElementById("Cats").style.display = "";
+}
+//About Me subcategories
+function displayAboutMeWho() {
+  document.getElementById("AboutMeWho").style.display = "";
+  document.getElementById("AboutMeWhere").style.display = "none";
+  document.getElementById("AboutMeWhat").style.display = "none";
+}
+function displayAboutMeWhere() {
+  document.getElementById("AboutMeWho").style.display = "none";
+  document.getElementById("AboutMeWhere").style.display = "";
+  document.getElementById("AboutMeWhat").style.display = "none";
+}
+function displayAboutMeWhat() {
+  document.getElementById("AboutMeWho").style.display = "none";
+  document.getElementById("AboutMeWhere").style.display = "none";
+  document.getElementById("AboutMeWhat").style.display = "";
 }
